98250a4abb6703f23e73bad32e2eb528d3f59ed6
Randell <piszmogcode@gmail.com>
2024-04-16 22:05:34 -0600

    Add more tests!

--- e2e/signin_test.go
+++ e2e/signin_test.go
@@ -22,3 +22,25 @@ func TestSignin(t *testing.T) {
 		Timeout: playwright.Float(10000),
 	}))
 }
+
+func TestSignin_InvalidCredentials(t *testing.T) {
+	beforeEach(t)
+	_, err := page.Goto(getFullPath("signin"))
+	require.NoError(t, err)
+
+	require.NoError(t, page.Locator("#email").Fill("user_does_not_exist@email.com"))
+	require.NoError(t, page.Locator("#password").Fill("password"))
+	require.NoError(t, page.Locator("button[type=submit]").Click())
+
+	require.NoError(t, expect.Locator(page.GetByText("Incorrect email or password")).ToBeVisible())
+}
+
+func TestSignin_AuthMiddleware(t *testing.T) {
+	beforeEach(t)
+	_, err := page.Goto(getFullPath("") + "/")
+	require.NoError(t, err)
+
+	require.NoError(t, expect.Page(page).ToHaveURL(getFullPath("signin"), playwright.PageAssertionsToHaveURLOptions{
+		Timeout: playwright.Float(10000),
+	}))
+}
