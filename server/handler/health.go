40c8383ae79cff77101da8a5c11ac9cb5fb810f1
Randell <piszmogcode@gmail.com>
2025-07-20 16:24:50 -0600

    Add health endpoint

---
+++ server/handler/health.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Piszmog/pathwise/version"
+)
+
+type healthResponse struct {
+	Status  string `json:"status"`
+	Version string `json:"version"`
+}
+
+func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
+	response := healthResponse{
+		Status:  "ok",
+		Version: version.Value,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		h.Logger.Error("failed to encode health response", "error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
