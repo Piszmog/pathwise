2e98e688393456549021223ce389f9bf3c4cd9ec
Randell <piszmogcode@gmail.com>
2025-07-20 19:41:33 -0600

    Simplify encoding

--- server/handler/health.go
+++ server/handler/health.go
@@ -18,16 +18,7 @@ func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 		Version: version.Value,
 	}
 
-	data, err := json.Marshal(response)
-	if err != nil {
-		h.Logger.Error("failed to marshal health response", "error", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(data); err != nil {
-		h.Logger.Error("failed to write health response", "error", err)
-	}
+	json.NewEncoder(w).Encode(response)
 }
