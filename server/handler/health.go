d62296697982e2f6737ddd0c3b937ba861ad7975
Randell <piszmogcode@gmail.com>
2025-07-20 19:43:41 -0600

    Add error handling

--- server/handler/health.go
+++ server/handler/health.go
@@ -20,5 +20,7 @@ func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		h.Logger.Error("failed to encode health response", "error", err)
+	}
 }
