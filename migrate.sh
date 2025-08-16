d226f957a1c7265aa5db82ba7fe05eeba927d0e3
Randell <piszmogcode@gmail.com>
2025-08-15 22:06:18 -0600

    Sneaky

--- migrate.sh
+++ migrate.sh
@@ -100,7 +100,7 @@ run_migration() {
 
     if [ "$PROTOCOL" == "libsql" ]; then
         if [ "$DIRECTION" == "down" ]; then
-            echo "Running libsql down"
+            echo "Running libsql down1"
             migrate-libsql \
                 -url "$PROTOCOL://$DB_URL" \
                 -token "$AUTH_TOKEN" \
@@ -108,7 +108,7 @@ run_migration() {
                 -direction down \
                 -steps "$STEPS"
         else
-            echo "Running libsql up"
+            echo "Running libsql up2"
             migrate-libsql \
                 -url "$PROTOCOL://$DB_URL" \
                 -token "$AUTH_TOKEN" \
@@ -117,13 +117,13 @@ run_migration() {
         fi
     else
         if [ "$DIRECTION" == "down" ]; then
-            echo "Running sqlite3 up"
+            echo "Running sqlite3 up3"
             migrate \
                 -source file://./internal/db/migrations \
                 -database "$FULL_DB_URL" \
                 down "$STEPS"
         else
-            echo "Running sqlite3 up"
+            echo "Running sqlite3 up4"
             migrate \
                 -source file://./internal/db/migrations \
                 -database "$FULL_DB_URL" \
