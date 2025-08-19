806813570938c8b9a9f74aca9ef57962a80333be
Randell <piszmogcode@gmail.com>
2025-08-18 21:07:58 -0600

    Fix sync interval

--- cmd/mcp/main.go
+++ cmd/mcp/main.go
@@ -35,7 +35,7 @@ func main() {
 			SyncURL:       os.Getenv("DB_PRIMARY_URL"),
 			Token:         os.Getenv("DB_TOKEN_READONLY"),
 			EncryptionKey: os.Getenv("ENC_KEY"),
-			SyncInterval:  time.Hour,
+			SyncInterval:  6 * time.Hour,
 		},
 	)
 	if err != nil {
